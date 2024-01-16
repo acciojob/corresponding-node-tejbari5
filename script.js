7c7497bddb9f7a28474197dee3912587ccc00add
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 16:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function correspondingNode(root1, root2, node1) {
-  
+function correspondingNode(tree1, tree2, node1) {
+    const pathToNode=getPath(tree1,node1)
+
+    return getNodeFromPath(tree2, pathToNode);
+
+
 }
 
+function getPath(root, node) { const path = [];
+while (node !== root) {
+const parent = node.parentElement; const children = Array.from(parent.children); const nodeIndex = children.indexOf(node);
+path.push(nodeIndex);
+node = parent;
+}
+return path;
+}
+function getNodeFromPath(node, path) { while (path.length > 0) {
+node = node.children[path.pop()];
+}
+return node;
+}
+
+
+  
 module.exports=correspondingNode;
